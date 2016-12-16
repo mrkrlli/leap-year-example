b94b42963cd5d7c7e55b2b60dc1a6080655e1a8a
Mark Li <mark@philosophie.is>
2016-12-16 10:43:29 -0800

    Refactor tests

--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -3,7 +3,7 @@ require "leap_year"
 describe LeapYear do
   describe '.leap_year?' do
 
-    context 'given a year that is evenly divisible by 4' do
+    context 'given a year that is evenly divisible by 4, but not by 100 or 400' do
       it 'should return true' do
         year = 8
 
@@ -11,25 +11,25 @@ describe LeapYear do
 
         expect(leap_year).to be true
       end
+    end
 
-      context 'where that year is evenly divisble by 100' do
-        it 'should return false' do
-          year = 1900
+    context 'given a year is evenly divisble by 100, but not by 400' do
+      it 'should return false' do
+        year = 1900
 
-          leap_year = LeapYear.leap_year?(year)
+        leap_year = LeapYear.leap_year?(year)
 
-          expect(leap_year).to be false
-        end
+        expect(leap_year).to be false
+      end
+    end
 
-        context 'given that year is evenly divisble by 400' do
-          it 'should return false' do
-            year = 2000
+    context 'given that year is evenly divisble by 400' do
+      it 'should return false' do
+        year = 2000
 
-            leap_year = LeapYear.leap_year?(year)
+        leap_year = LeapYear.leap_year?(year)
 
-            expect(leap_year).to be true
-          end
-        end
+        expect(leap_year).to be true
       end
     end
 
