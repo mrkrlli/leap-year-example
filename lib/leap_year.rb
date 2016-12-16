468337372b7645b5edf33e26c0375bb47dacf75c
Mark Li <mark@philosophie.is>
2016-12-16 10:18:03 -0800

    Add functionality for years not evenly divisible by 4

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,5 +1,7 @@
 class LeapYear
   def self.leap_year?(year)
+    return false if year % 4 != 0
+
     return true if year % 4 == 0
   end
 end
--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -2,6 +2,7 @@ require "leap_year"
 
 describe LeapYear do
   describe '.leap_year?' do
+
     context 'given a year that is evenly divisible by 4' do
       it 'should return true' do
         year = 8
@@ -9,6 +10,16 @@ describe LeapYear do
         leap_year = LeapYear.leap_year?(year)
 
         expect(leap_year).to be true
+      end
+    end
+
+    context 'given a year that is not evenly divisible by 4' do
+      it 'should return false' do
+        year = 5
+
+        leap_year = LeapYear.leap_year?(year)
+
+        expect(leap_year).to be false
       end
     end
   end
