140ca118de0f1cb9655bfcf17c3c8ee4a0913512
Mark Li <mark@philosophie.is>
2016-12-16 10:30:13 -0800

    Add example of refactor given test suite

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -2,14 +2,10 @@ class LeapYear
   def self.leap_year?(year)
     return false if year % 4 != 0
 
-    if year % 4 == 0
-      if year % 100 == 0
-        return true if year % 400 == 0
+    return true if year % 400 == 0
 
-        return false
-      end
+    return false if year % 100 == 0
 
-      return true
-    end
+    return true if year % 4 == 0
   end
 end
